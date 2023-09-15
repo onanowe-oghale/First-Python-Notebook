dbf71db8d82f1ae8ded87329741bebfe9bec5378
onanowe-oghale <83960706+onanowe-oghale@users.noreply.github.com>
2023-09-15 14:23:19 +0100

    Going strong🦅

---
+++ Learn Python the Hardway scripts/ex27.py
@@ -0,0 +1,53 @@
+#Memorizing Logic
+
+"""
+Truth terms
+#-----------------------
+and 
+or
+not
+
+
+!= : not equal
+== :equal to
+>= :greater than or equal
+<=  :Less than or equal
+
+
+
+True
+False
+"""
+
+
+#--------------------------------------How the logic works------------------------------------------
+
+"""
+NOT: This negates whatever inputs it gets as not can only be one input so if it has on input of 1 (on) it gives an output of 0 (off)
+
+OR: This gives an output of true if at least one output is 1 or true and the other possiblities would be false or 0 unlike the previous NOT gate the OR accepts two or more inputs
+
+AND: This gives an output of true as far as both inputs are true or one leaving the rest as false.
+
+NAND: NOT AND the NOT simply negates the AND and makes it the reverse of everything here every output is true except for when all are true
+
+NOR: This negates the OR just like NAND seen previously the NOT makes all outputs false except from from when all inputs are false 
+"""
+
+#-------------------------------_______________________--------------------------
+
+"""
+1 != 0 -------True
+1 != 1--------False
+0 != 1--------True
+0 != 1--------False.
+
+
+
+==
+
+1 == 0-------False
+1 == 1-------True
+0 == 1-------False
+0 == 0-------True
+"""
---
+++ Learn Python the Hardway scripts/ex29_29.py
@@ -0,0 +1,17 @@
+people = 25
+cats = 334
+dogs = 12
+
+if people and cats :
+    print("Mad oh")    
+
+
+if people or cats :
+    print("Getting more knowledge I see")
+
+
+dogs -= 5
+print("The initial dog value has been restored to!", dogs)
+
+if dogs != people:
+    print("people aren't equal to them dogs then")
